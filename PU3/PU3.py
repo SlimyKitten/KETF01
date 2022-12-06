be235e64f0aed86380d762cb46ae4d748f14fc9c
Witko Tamm <witko@tamm-tamm.de>
2022-12-06 12:08:02 +0100

    Model with exp_data

--- PU3/PU3.py
+++ PU3/PU3.py
@@ -2,9 +2,14 @@ import numpy as np
 from scipy.constants import R,pi
 from scipy.integrate import solve_ivp 
 from matplotlib import pyplot as plt
+import csv
 rho_L=998.2 #densitet @20degC
 rho_G=1.189
 g=9.82
+H = 82547
+atm = 101325
+p_O2 = 0.21*atm
+c_max = p_O2/H
 
 def size_bubble(d0=0.1E-3):
     sigma = 0.072
@@ -14,17 +19,21 @@ def terminal_rise_velocity(d_bubble=size_bubble()):
     C_D=0.5
     return ((4*d_bubble*(rho_L-rho_G)*g)/(3*C_D*rho_L))**(1/2)
 
+def read_CSV(fp):
+    with open(fp, 'r') as data_file:
+        reader = csv.reader(data_file)
+        next(reader)
+        data = [data for data in reader]
+        return data
+
 def model(t, c):
     h=0.2
     q_g = 2E-3/60
     D_O2_Luft = 1.76E-5
     D_02_H2O = 1.8E-9
     delta_x=0.1E-3
-    H = 82547
-    atm = 101325
     d_tank = 0.16
     V = h*(d_tank/2)**2*pi
-    p_O2 = 0.21*atm
     k_G = D_O2_Luft/(R*298.15*delta_x)
     k_L = D_02_H2O/delta_x
     K_L = (1/k_L+1/(H*k_G))**-1
@@ -36,14 +45,19 @@ def model(t, c):
     num_bubbles = q_g*h/(vol_b*v_t)
     A=num_bubbles*pi*d_bubble**2
     dcdt = A*N_a/V
+
     return dcdt
 
 # print(model(0,0))
 
-t_span = [0,5*60*60]
+t_span = [0,5*60]
 c0=[0]
 sol = solve_ivp(model,t_span,c0)
 print(sol.t)
-plt.plot(sol.t, sol.y.T, label='Modelldata')
-
+plt.plot(sol.t, sol.y.T/c_max*100, label='Modelldata')
+exp_data = read_CSV('PU3_exp_data.csv')
+t, c, T = np.asarray(exp_data, dtype=np.float32).T
+plt.plot(t, c,'k*')
+plt.xlabel('Tid [sekunder]')
+plt.ylabel('Koncentration [mol/liter]')
 plt.show()
